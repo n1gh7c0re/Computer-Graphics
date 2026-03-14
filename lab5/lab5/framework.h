5a3524a06cd704db964bff08d56e9464936e15d4
Sergey <159152648+n1gh7c0re@users.noreply.github.com>
2026-03-14 14:39:35 +0300

    add lab5

---
+++ lab5/lab5/framework.h
@@ -0,0 +1,160 @@
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN    
+#define NOMINMAX
+
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <assert.h>
+
+#include <string>
+#include <vector>
+
+#include <dxgi.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+#include <DirectXMath.h>
+
+#include <chrono>
+
+#define MAX_LOADSTRING 100
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
+#include "Matrix.h"
+#include "DDS.h"
+
+#define SAFE_RELEASE(p)\
+{\
+    if (p != nullptr)\
+    {\
+        p->Release();\
+        p = nullptr;\
+    }\
+}
+
+inline HRESULT SetResourceName(ID3D11DeviceChild* pResource, const std::string& name)
+{
+    return pResource->SetPrivateData(WKPDID_D3DDebugObjectName, (UINT)name.length(), name.c_str());
+}
+
+inline std::wstring Extension(const std::wstring& filename)
+{
+    size_t dotPos = filename.rfind(L'.');
+    if (dotPos != std::wstring::npos)
+    {
+        return filename.substr(dotPos + 1);
+    }
+    return L"";
+}
+
+inline std::string WCSToMBS(const std::wstring& wstr)
+{
+    size_t len = wstr.length();
+
+    // We suppose that on Windows platform wchar_t is 2 byte length
+    std::vector<char> res;
+    res.resize(len + 1);
+
+    size_t resLen = 0;
+    wcstombs_s(&resLen, res.data(), res.size(), wstr.c_str(), len);
+
+    return res.data();
+}
+
+template <typename T>
+T DivUp(const T& a, const T& b)
+{
+    return (a + b - (T)1) / b;
+}
+
+inline UINT32 GetBytesPerBlock(const DXGI_FORMAT& fmt)
+{
+    switch (fmt)
+    {
+    case DXGI_FORMAT_BC1_TYPELESS:
+    case DXGI_FORMAT_BC1_UNORM:
+    case DXGI_FORMAT_BC1_UNORM_SRGB:
+    case DXGI_FORMAT_BC4_TYPELESS:
+    case DXGI_FORMAT_BC4_UNORM:
+    case DXGI_FORMAT_BC4_SNORM:
+        return 8;
+        break;
+
+    case DXGI_FORMAT_BC2_TYPELESS:
+    case DXGI_FORMAT_BC2_UNORM:
+    case DXGI_FORMAT_BC2_UNORM_SRGB:
+    case DXGI_FORMAT_BC3_TYPELESS:
+    case DXGI_FORMAT_BC3_UNORM:
+    case DXGI_FORMAT_BC3_UNORM_SRGB:
+    case DXGI_FORMAT_BC5_TYPELESS:
+    case DXGI_FORMAT_BC5_UNORM:
+    case DXGI_FORMAT_BC5_SNORM:
+    case DXGI_FORMAT_BC6H_TYPELESS:
+    case DXGI_FORMAT_BC6H_UF16:
+    case DXGI_FORMAT_BC6H_SF16:
+    case DXGI_FORMAT_BC7_TYPELESS:
+    case DXGI_FORMAT_BC7_UNORM:
+    case DXGI_FORMAT_BC7_UNORM_SRGB:
+        return 16;
+        break;
+    }
+    assert(0);
+    return 0;
+}
+
+struct Camera
+{
+    Point3f poi;
+    float r;
+    float phi;
+    float theta;
+};
+
+struct TextureVertex
+{
+    float x, y, z;
+    float u, v;
+};
+
+struct ColorVertex
+{
+    float x, y, z;
+    COLORREF color;
+};
+
+struct Vertex
+{
+    float x, y, z;
+    COLORREF color;
+};
+
+struct GeomBuffer
+{
+    DirectX::XMMATRIX m;
+};
+
+struct SceneBuffer
+{
+    DirectX::XMMATRIX vp;
+    Point4f cameraPos;
+};
+
+struct SphereGeomBuffer
+{
+    DirectX::XMMATRIX m;
+    Point4f size;
+};
+
+struct RectGeomBuffer
+{
+    DirectX::XMMATRIX m;
+    Point4f color;
+};
